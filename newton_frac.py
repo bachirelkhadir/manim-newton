320774165f4ae4375f5abd56f400fd142d834f0b
bachirelkhadir <bachir009+git@gmail.com>
2021-01-19 19:53:02 -0500

    .

--- newton_frac.py
+++ newton_frac.py
@@ -5,32 +5,80 @@ import numpy as np
 from scipy import linalg
 from scipy.stats import norm
 from manim import *
+from helper_functions import *
+
 
 config.max_files_cached = 10000
 config.background_color = "#1f303e"
 
-PROJECT_DIR = '/home/bachir/Dropbox/Job_Applications/Academia/Application_Fall_2020/Application_By_School/UCLouvain/Presentations/newton/manim/'
-sys.path.append(PROJECT_DIR)
-from helper_functions import *
-
+DEBUG = config.quality == "low_quality"
 
 f = lambda x: (x**3 - 2)/5.
 df = lambda x: 3/5*x**2
 x0 = 2.
 
-SIZE_GRID = 400
-ZOOM_LEVEL = 0
+SIZE_GRID = 200
+ZOOM_LEVEL = 1.
+RUN_TIME = .1
 COLORS_DICT = [YELLOW, BLUE, RED]
 
+cross = lambda: VGroup(Line(UP + LEFT, DOWN + RIGHT),
+                Line(UP + RIGHT, DOWN + LEFT)).scale(.1)
 class NewtonFractal(MovingCameraScene):
 
+    NUM_BLACK_SCREENS = 0
+    def wait(self, timeout=1):
+        self.NUM_BLACK_SCREENS += 1
+        print("Blackish Screen: ", self.NUM_BLACK_SCREENS)
+        color = BLACK
+        rect = Rectangle(fill_color=color, strole_color=color, fill_opacity=1).scale(100)
+        super(NewtonFractal, self).wait(timeout)
+
+        if not DEBUG:
+            self.add(rect)
+            super(NewtonFractal, self).wait(timeout)
+            self.remove(rect)
+
     def construct(self):
-        self.add_fractal()
+
+        self.camera_frame.scale(.7).shift(1.5*LEFT)
+        self.construct_grid()
+        self.grid.set_stroke(opacity=.05)
+
+        unit_circle   = Circle(WHITE, stroke_opacity=.5)
+
+        self.play(FadeIn(MathTex(r"z^3 - 1 = 0").to_corner(LEFT).shift(RIGHT/2)))
         self.wait()
-        self.grid.set_stroke(opacity=.1)
-        self.play(self.camera_frame.scale, ZOOM_LEVEL, run_time=4)
+        self.add_axes()
         self.wait()
-        return
+        self.play(ShowCreation(unit_circle))
+        self.wait()
+
+        label_groups = []
+        labels = list(map(MathTex, ["1", r"e^{\frac{2i\pi}3}", r"e^{\frac{4i\pi}3}"]))
+        next_to = [ DR, UL, DL ]
+        for theta, c, lab, n  in zip((0, 2*PI/3, 4*PI/3), COLORS_DICT, labels, next_to):
+            cr = cross().move_to(np.cos(theta) *RIGHT + np.sin(theta) * UP)
+            cr.set_color(c)
+            lab.next_to(cr, n, SMALL_BUFF).set_color(c)
+            label_groups.append(VGroup(cr, lab))
+
+        for lab in label_groups:
+            self.play(Write(lab))
+        self.wait()
+
+        for lab in label_groups:
+            self.remove(lab)
+        self.remove(unit_circle)
+        self.play(ShowCreation(Rectangle(width=4, height=4)))
+        self.wait()
+
+        self.play(ShowCreation(self.grid))
+        self.remove(self.ax_objects)
+        self.wait()
+
+        self.grid.set_stroke(opacity=0)
+        self.wait()
 
     def get_grid(self, n, m, height=4):
         grid = VGroup(*[
@@ -43,27 +91,30 @@ class NewtonFractal(MovingCameraScene):
         grid.set_stroke(WHITE, 2)
         return grid
 
-    def add_fractal(self):
-        x_min, x_max = -2, 3
-        y_min, y_max = -2, 2
-
-        x_axis, y_axis, coords_to_point = axes = add_2d_axes(x_min, x_max, y_min, y_max)
-
-        cross = lambda: VGroup(Line(UP + LEFT, DOWN + RIGHT),
-                Line(UP + RIGHT, DOWN + LEFT)).scale(.1)
+    def construct_grid(self):
 
         m = np.load(open("newton_fractal.np", "rb"))
-        grid = self.grid = self.get_grid(SIZE_GRID, SIZE_GRID, ZOOM_LEVEL/2  * config.frame_width)
-        offset = 0
+        grid = self.grid = self.get_grid(SIZE_GRID, SIZE_GRID)
+        offset = int((len(m) - SIZE_GRID) / 2)
+        print("OFFSET:", offset)
         for i in range(SIZE_GRID):
             for j in range(SIZE_GRID):
                 grid[i][j].set_opacity(.5)
                 grid[i][j].set_color(COLORS_DICT[int(m[i+offset, j+offset])])
 
-        self.add(x_axis, y_axis)
-        self.add(MathTex("Re(x)").next_to(x_axis, RIGHT))
-        self.add(MathTex("Im(x)").next_to(y_axis, UP))
-        self.add(grid)
+    def add_axes(self):
+        x_min, x_max = -2, 2
+        y_min, y_max = -2, 2
+
+        x_axis, y_axis, coords_to_point = axes = add_2d_axes(x_min, x_max, y_min, y_max)
+
+
+        self.ax_objects = VGroup(
+            x_axis, y_axis,
+            MathTex(r"\Re(z)").next_to(x_axis, RIGHT),
+            MathTex(r"\Im(z)").shift(2.3*UP))
+        self.add(self.ax_objects)
+
 
 # Local Variables:
 # bachir-prog-local-mode: t
